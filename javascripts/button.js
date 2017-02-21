9089c7a9570d58b6808944ba3525af996c612733
itsmeyuleek <yuekorobkova@365w418280.local>
2017-02-21 14:48:44 +0300

    list

--- javascripts/button.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  new Dragdealer('flat-slider');
-
-  var globalCounter = 0;
-  $('.button').click(function(e) {
-    e.preventDefault();
-    $('.global-counter').text(++globalCounter);
-  });
-});
